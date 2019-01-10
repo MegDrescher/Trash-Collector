8ef8137d8b3b979e12b62969174feb0008b206e1
unknown <megdrescher@icloud.com>
2019-01-10 09:26:36 -0600

    removed file and made changes to identity model

---
+++ TrashCollector/Models/IdentityModels.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TrashCollector.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<Zipcode> Zipcodes { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<PickupDay> PickupDays{ get; set; }
+
+        public DbSet<TodaysPickup> TodaysPickup { get; set; }
+
+        public DbSet<RunUpdate> RunUpdates{ get; set; }
+
+
+    }
+}
