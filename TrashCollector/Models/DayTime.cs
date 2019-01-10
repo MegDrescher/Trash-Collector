40713a1dcd5c904030a59a5b7e3f2ec5a3980c19
unknown <megdrescher@icloud.com>
2019-01-10 11:17:34 -0600

    DayTime class model with Primary Key

--- TrashCollector/Models/DayTime.cs
+++ TrashCollector/Models/DayTime.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,10 @@ namespace TrashCollector.Models
 {
     public class DayTime
     {
+        [Key]
+
+        public int Id { get; set; }
+        public string TimeOfDay { get; set; }
+
     }
 }
