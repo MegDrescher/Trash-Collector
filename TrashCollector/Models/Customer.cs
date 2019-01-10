40102c1a5f7f874a9615d75a468a6688e603da95
unknown <megdrescher@icloud.com>
2019-01-10 11:15:39 -0600

    Added DayTime Class. Added properties to Employee and Customer Model

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -21,7 +21,7 @@ namespace TrashCollector.Models
         public string Email { get; set; }
 
         //[Display(PhoneNumber ="Phone Number")]
-        public string PhoneNumber { get; set; }
+        //public string PhoneNumber { get; set; }
         public string Address { get; set; }
 
         [ForeignKey("City")]
@@ -40,6 +40,42 @@ namespace TrashCollector.Models
         public int? ZipcodeId { get; set; }
         public Zipcode zipcode { get; set; }
 
+        [ForeignKey("Residence")]
+        public int? ResidenceTypeId { get; set; }
+        public Residence residence { get; set; }
+
+        public double? latitude { get; set; }
+        public double? longitude { get; set; }
+
+        [ForeignKey("PickUpDay")]
+        [Display(Name = "Pick Up Day")]
+        public int? PickUpDayId { get; set; }
+        [ForeignKey ("ExtraPickUpDay")]
+        [Display(Name ="Requested Extra Pick Up Day")]
+        public int? ExtraRequestedDayId { get; set; }
+        
+        public PickupDay ExtraPickUpDay { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.###}")]
+        public double? amountDue { get; set; }
+        public DateTime DueDate { get; set; }
+        [Display(Name ="Total Paid")]
+        public double? TotalPaid { get; set; }
+
+        [ForeignKey ("ApplicationUser")]
+        public string UserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public IEnumerable<Residence> Residences { get; set; }
+        //public IEnumerable<DayTime> DayTimes { get; set; }
+        public IEnumerable<Zipcode> ZipCode { get; set; }
+        public IEnumerable<City> Cities { get; set; }   
+        public IEnumerable<State> States { get; set; }
+        public IEnumerable <PickupDay> PickUpDays { get; set; }
+
+
+        
+
+
+        
        
 
 
--- TrashCollector/Models/Employee.cs
+++ TrashCollector/Models/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,31 @@ namespace TrashCollector.Models
 {
     public class Employee
     {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        [Display(Name = "User Name")]
+        public string Email { get; set; }
+        public DayTime DOB { get; set; }
+        public string PhoneNumber { get; set; }
+        [Display(Name ="Phone Number")]
+
+
+
+        [ForeignKey("Zipcode")]
+        //[Display(zipcode ="Zip Code")]
+        public int? ZipcodeId { get; set; }
+        public Zipcode zipcode { get; set; }
+
+        public IEnumerable<Zipcode> ZipCode { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
     }
+
 }
